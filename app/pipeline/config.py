1b31e6c4e204c0d2da66416f40c3b0f3c289e6c4
kutpiotr <kutpiotr1@gmail.com>
2026-03-14 13:51:20 +0100

    Dodanie konfiguracji miast i parametrów

---
+++ app/pipeline/config.py
@@ -0,0 +1,6 @@
+CITIES=[
+{"name":"Rzeszow","latitude":50.0413,"longitude":21.9990},
+{"name":"Krakow","latitude":50.0647,"longitude":19.9450},
+{"name":"Warsaw","latitude":52.2297,"longitude":21.0122},
+{"name":"Gdansk","latitude":54.3520,"longitude":18.6466}
+]
--- app/pipeline/run_pipeline.py
+++ app/pipeline/run_pipeline.py
@@ -1,21 +1,33 @@
+import pandas as pd
+
+from config import CITIES
 from extract import fetch_weather_data
 from transform import transform_weather_data
 from load import load_to_sqlite
 
-CITY_NAME="Rzeszow"
-LATITUDE=50.0413
-LONGITUDE=21.9990
-
 def main():
-    raw_data=fetch_weather_data(latitude=LATITUDE,longitude=LONGITUDE)
-    df=transform_weather_data(raw_data=raw_data,city_name=CITY_NAME)
+    dataframes=[]
 
-    print("Transformed data:")
-    print(df)
+    for city in CITIES:
+        city_name=city["name"]
+        latitude=city["latitude"]
+        longitude=city["longitude"]
 
-    load_to_sqlite(df)
+        print(f"Fetching data for: {city_name}")
 
-    print(f"\nLoaded {len(df)} rows into database.")
+        raw_data=fetch_weather_data(latitude=latitude,longitude=longitude)
+        city_df=transform_weather_data(raw_data=raw_data,city_name=city_name)
+
+        dataframes.append(city_df)
+
+    final_df=pd.concat(dataframes,ignore_index=True)
+
+    print("\nCombined data:")
+    print(final_df)
+
+    load_to_sqlite(final_df)
+
+    print(f"\nLoaded {len(final_df)} rows into database.")
 
 if __name__=="__main__":
     main()
