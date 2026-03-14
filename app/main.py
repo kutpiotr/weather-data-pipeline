67b6cf074065d594f4ff6b9e66e36bfd3cb66ada
kutpiotr <kutpiotr1@gmail.com>
2026-03-14 13:20:05 +0100

    Parsowanie JSON do pandas.DataFrame

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 
 BASE_URL="https://api.open-meteo.com/v1/forecast"
 
@@ -10,15 +11,27 @@ params={
 }
 
 response=requests.get(BASE_URL,params=params,timeout=10)
-
 print("Status code:",response.status_code)
 
 response.raise_for_status()
 
 data=response.json()
+daily_data=data["daily"]
 
-print("\nAvailable top-level keys:")
-print(data.keys())
+df=pd.DataFrame({
+"date":daily_data["time"],
+"temperature_max":daily_data["temperature_2m_max"],
+"temperature_min":daily_data["temperature_2m_min"],
+"precipitation_sum":daily_data["precipitation_sum"]
+})
 
-print("\nDaily data:")
-print(data["daily"])
+df["date"]=pd.to_datetime(df["date"])
+
+print("\nDataFrame preview:")
+print(df)
+
+print("\nDataFrame info:")
+print(df.info())
+
+print("\nBasic statistics:")
+print(df.describe())
