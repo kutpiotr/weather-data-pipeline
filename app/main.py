77d7998a04a0f9056a660b49f563f9ba259386a9
kutpiotr <kutpiotr1@gmail.com>
2026-03-14 13:17:18 +0100

    połączenie z API i test pobierania danych

--- app/main.py
+++ app/main.py
@@ -1,6 +1,24 @@
 import requests
-import pandas as pd
-import streamlit as st
 
-print("Environment is ready.")
-print("requests, pandas and streamlit imported successfully.")
+BASE_URL="https://api.open-meteo.com/v1/forecast"
+
+params={
+"latitude":50.0413,
+"longitude":21.9990,
+"daily":"temperature_2m_max,temperature_2m_min,precipitation_sum",
+"timezone":"Europe/Warsaw"
+}
+
+response=requests.get(BASE_URL,params=params,timeout=10)
+
+print("Status code:",response.status_code)
+
+response.raise_for_status()
+
+data=response.json()
+
+print("\nAvailable top-level keys:")
+print(data.keys())
+
+print("\nDaily data:")
+print(data["daily"])
